c21fc383803224a125d8748366508ca39d2a2d05
Aolderr18 <Aolderr18@gmail.com>
2016-03-20 16:19:50 -0500

    Create InvalidChessMove.java

---
+++ InvalidChessMove.java
@@ -0,0 +1,10 @@
+package Chess;
+
+/**
+ * This exception is thrown if a player attemps to make an illegal chess move.
+ */
+public class InvalidChessMoveException extends Exception {
+	public InvalidChessMoveException() {
+
+	}
+}
